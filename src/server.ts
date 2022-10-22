14bbf1bfdf92560477542728d5dabc11b300887d
Enos Gabriel <enosgb55@gmail.com>
2022-10-22 17:09:34 -0300

    build project

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
-import {Router,Response,Request,NextFunction} from "express";
+import { Router, Response, Request, NextFunction } from "express";
 import routes from "./Routes/index";
 import db from "./Config/dbConnect";
+import 'express-async-errors'
+import 'reflect-metadata'
 const express = require('express')
 
 
@@ -24,9 +26,9 @@ const port = process.env.MONGO_URI;
 
 app.use(express.json());
 
-app.use(function(req:any, res:Response, next:NextFunction) {
+app.use(function (req: any, res: Response, next: NextFunction) {
   if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
-    jwt.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', function(err:any, decode:any) {
+    jwt.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', function (err: any, decode: any) {
       if (err) req.user = undefined;
       req.user = decode;
       next();
@@ -39,4 +41,4 @@ app.use(function(req:any, res:Response, next:NextFunction) {
 
 routes(app);
 
-app.listen(port, () => `server running on port ${port}` );
+app.listen(port, () => `server running on port ${port}`);
